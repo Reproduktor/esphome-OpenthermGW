e26bbdfede69213dc326204e25b0fc9656830318
marek.fekete <marek.fekete@siemens.healthineers.com>
2023-10-01 17:37:43 +0200

    Initial version, with basic Opentherm loop

---
+++ components/opentherm/__init__.py
@@ -0,0 +1,65 @@
+CODEOWNERS = ["@reproduktor/esphome-OpenthermGW"]
+
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import climate, sensor
+from esphome import pins
+from esphome.const import (
+    CONF_ID,
+)
+from esphome.core import coroutine_with_priority
+from esphome.cpp_generator import RawExpression
+
+CONF_THERMOSTAT_IN_PIN = "thermostat_in_pin"
+CONF_THERMOSTAT_OUT_PIN = "thermostat_out_pin"
+CONF_BOILER_IN_PIN = "boiler_in_pin"
+CONF_BOILER_OUT_PIN = "boiler_out_pin"
+CONF_OPENTHERM_ID = "opentherm_id"
+
+opentherm_ns = cg.esphome_ns.namespace("OPENTHERM")
+OpenThermGW = opentherm_ns.class_("OpenthermGW", cg.Component, cg.esphome_ns.namespace("text_sensor").class_("TextSensor"))
+
+
+MULTI_CONF = False
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(OpenThermGW),
+        cv.Required(CONF_THERMOSTAT_IN_PIN): pins.internal_gpio_input_pin_number,
+        cv.Required(CONF_THERMOSTAT_OUT_PIN): pins.internal_gpio_input_pin_number,
+        cv.Required(CONF_BOILER_IN_PIN): pins.internal_gpio_input_pin_number,
+        cv.Required(CONF_BOILER_OUT_PIN): pins.internal_gpio_input_pin_number,
+    }
+).extend(cv.COMPONENT_SCHEMA)
+
+
+@coroutine_with_priority(2.0)
+async def to_code(config):
+
+    cg.add_global(opentherm_ns.using)
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+
+    cg.add(var.set_thermostat_in_pin(config[CONF_THERMOSTAT_IN_PIN]))
+    cg.add(var.set_thermostat_out_pin(config[CONF_THERMOSTAT_OUT_PIN]))
+    cg.add(var.set_boiler_in_pin(config[CONF_BOILER_IN_PIN]))
+    cg.add(var.set_boiler_out_pin(config[CONF_BOILER_OUT_PIN]))
+
+    #cg.add_library("ihormelnyk/OpenTherm Library", "1.1.3")
+    #cg.add_library("https://github.com/rsciriano/opentherm_library.git#functional_callbacks", None)
+
+def opentherm_component_schema():
+    """Create a schema for a OpenTherm component.
+    :return: The OpenTherm component schema, `extend` this in your config schema.
+    """
+    schema = {
+        cv.GenerateID(CONF_OPENTHERM_ID): cv.use_id(OpenThermGW),
+    }
+    return cv.Schema(schema)
+
+#async def set_hotwater_climate(var, config):
+#    parent = await cg.get_variable(config[CONF_OPENTHERM_ID])
+#    cg.add(parent.set_hotWaterClimate(var))
+
+#async def set_heatingwater_climate(var, config):
+#    parent = await cg.get_variable(config[CONF_OPENTHERM_ID])
+#   cg.add(parent.set_heatingWaterClimate(var))
---
+++ components/opentherm/openthermgw.h
@@ -0,0 +1,103 @@
+#include "esphome.h"
+//#include "esphome/components/sensor/sensor.h"
+#include "OpenTherm.h"
+
+namespace esphome {
+namespace openthermgw {
+
+class OpenthermGW: public PollingComponent
+{
+    private:
+    const char *TAG = "openthermgw_component";
+    OpenTherm *ot;
+    protected:
+    uint8_t thermostat_in_pin_ = -1;
+    uint8_t thermostat_out_pin_ = -1;
+    uint8_t boiler_in_pin_ = -1;
+    uint8_t boiler_out_pin_ = -1;
+
+    #define MODE_LISTEN_MASTER 0
+    #define MODE_LISTEN_SLAVE 1
+
+    int mode = 0;
+    OpenthermData message;
+
+    public:
+    void set_thermostat_in_pin(uint8_t pin) { thermostat_in_pin_ = pin; }
+    void set_thermostat_out_pin(uint8_t pin) { thermostat_out_pin_ = pin; }
+    void set_boiler_in_pin(uint8_t pin) { boiler_in_pin_ = pin; }
+    void set_boiler_out_pin(uint8_t pin) { boiler_out_pin_ = pin; }
+    
+    OpenthermGW(): PollingComponent(3000)
+    {
+    }
+
+    void setup() override
+    {
+        // This will be called once to set up the component
+        // think of it as the setup() call in Arduino
+        ESP_LOGD("openthermGW_component", "Setup");
+
+        pinMode(thermostat_in_pin_, INPUT);
+        digitalWrite(thermostat_in_pin_, HIGH); // pull up
+        digitalWrite(thermostat_out_pin_, HIGH);
+        pinMode(thermostat_out_pin_, OUTPUT); // low output = high current, high output = low current
+        pinMode(boiler_in_pin_, INPUT);
+        digitalWrite(boiler_in_pin_, HIGH); // pull up
+        digitalWrite(boiler_out_pin_, HIGH);
+        pinMode(boiler_out_pin_, OUTPUT); // low output = high voltage, high output = low voltage
+    }
+
+    void update() override
+    {
+        ESP_LOGD("openthermgw_component", "update");
+    }
+
+    void loop() override
+    {
+        if (mode == MODE_LISTEN_MASTER)
+        {
+            if (OPENTHERM::isSent() || OPENTHERM::isIdle() || OPENTHERM::isError())
+            {
+                OPENTHERM::listen(thermostat_in_pin_);
+            }
+            else if (OPENTHERM::getMessage(message))
+            {
+                //Serial.print(F("-> "));
+                //OPENTHERM::printToSerial(message);
+                //Serial.println();
+                ESP_LOGD("openthermGW_component", "Message from thermostat: %1", message.u16);
+
+                OPENTHERM::send(boiler_out_pin_, message); // forward message to boiler
+                mode = MODE_LISTEN_SLAVE;
+            }
+        }
+        else if (mode == MODE_LISTEN_SLAVE)
+        {
+            if (OPENTHERM::isSent())
+            {
+                OPENTHERM::listen(boiler_in_pin_, 800); // response need to be send back by boiler within 800ms
+            }
+            else if (OPENTHERM::getMessage(message))
+            {
+                //Serial.print(F("<- "));
+                //OPENTHERM::printToSerial(message);
+                //Serial.println();
+                //Serial.println();
+                ESP_LOGD("openthermGW_component", "Message from boiler: %1", message.u16);
+                OPENTHERM::send(thermostat_out_pin_, message); // send message back to thermostat
+                mode = MODE_LISTEN_MASTER;
+            }
+            else if (OPENTHERM::isError())
+            {
+                ESP_LOGD("openthermGW_component", "Message error");
+                mode = MODE_LISTEN_MASTER;
+                // Serial.println(F("<- Timeout"));
+                // Serial.println();
+            }
+        }
+    }
+};
+
+}
+}
