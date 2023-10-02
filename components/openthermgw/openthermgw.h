047202535a8b14a6bd5930e52aafff579ecdb1fd
marek.fekete <marek.fekete@siemens.healthineers.com>
2023-10-02 10:36:40 +0200

    v13: trying to remove logs during opentherm message handling

--- components/openthermgw/openthermgw.h
+++ components/openthermgw/openthermgw.h
@@ -8,7 +8,7 @@ namespace openthermgw {
 class OpenthermGW: public PollingComponent
 {
     private:
-    const char *LOGTOPIC = "openthermgw_component_12";
+    const char *LOGTOPIC = "openthermgw_component_13";
 
     protected:
     uint8_t master_in_pin_ = -1;
@@ -34,7 +34,7 @@ class OpenthermGW: public PollingComponent
     void set_slave_in_pin(uint8_t pin) { slave_in_pin_ = pin; }
     void set_slave_out_pin(uint8_t pin) { slave_out_pin_ = pin; }
     
-    OpenthermGW(): PollingComponent(3000)
+    OpenthermGW(): PollingComponent(10000)
     {
     }
 
@@ -52,15 +52,15 @@ class OpenthermGW: public PollingComponent
         digitalWrite(slave_in_pin_, HIGH); // pull up
         digitalWrite(slave_out_pin_, HIGH);
         pinMode(slave_out_pin_, OUTPUT); // low output = high voltage, high output = low voltage
+        master_in_pin_sensor->publish_state(master_in_pin_);
+        master_out_pin_sensor->publish_state(master_out_pin_);
+        slave_in_pin_sensor->publish_state(slave_in_pin_);
+        slave_out_pin_sensor->publish_state(slave_out_pin_);
     }
 
     void update() override
     {
         ESP_LOGD(LOGTOPIC, "update");
-        master_in_pin_sensor->publish_state(master_in_pin_);
-        master_out_pin_sensor->publish_state(master_out_pin_);
-        slave_in_pin_sensor->publish_state(slave_in_pin_);
-        slave_out_pin_sensor->publish_state(slave_out_pin_);
 
     }
 
@@ -77,9 +77,9 @@ class OpenthermGW: public PollingComponent
                 //Serial.print(F("-> "));
                 //OPENTHERM::printToSerial(message);
                 //Serial.println();
-                ESP_LOGD(LOGTOPIC, "Message from thermostat type: %02hhx  id: %02hhx  HB: %02hhx  LB: %02hhx", message.type, message.id, message.valueHB, message.valueLB);
+                //ESP_LOGD(LOGTOPIC, "Message from thermostat type: %02hhx  id: %02hhx  HB: %02hhx  LB: %02hhx", message.type, message.id, message.valueHB, message.valueLB);
                 OPENTHERM::send(slave_out_pin_, message); // forward message to boiler
-                ESP_LOGD(LOGTOPIC, "---> sent to boiler");//: %1", message.id);
+                //ESP_LOGD(LOGTOPIC, "---> sent to boiler");//: %1", message.id);
                 mode = MODE_LISTEN_SLAVE;
             }
         }
@@ -87,9 +87,9 @@ class OpenthermGW: public PollingComponent
         {
             if (OPENTHERM::isSent())
             {
-                ESP_LOGD(LOGTOPIC, "waiting for response from boiler...");
+                //ESP_LOGD(LOGTOPIC, "waiting for response from boiler...");
                 OPENTHERM::listen(slave_in_pin_, 800); // response need to be send back by boiler within 800ms
-                ESP_LOGD(LOGTOPIC, "... finished waiting.");
+                //ESP_LOGD(LOGTOPIC, "... finished waiting.");
             }
             else if (OPENTHERM::getMessage(message))
             {
@@ -97,9 +97,9 @@ class OpenthermGW: public PollingComponent
                 //OPENTHERM::printToSerial(message);
                 //Serial.println();
                 //Serial.println();
-                ESP_LOGD(LOGTOPIC, "Message from boiler type: %02hhx  id: %02hhx  HB: %02hhx  LB: %02hhx", message.type, message.id, message.valueHB, message.valueLB);
+                //ESP_LOGD(LOGTOPIC, "Message from boiler type: %02hhx  id: %02hhx  HB: %02hhx  LB: %02hhx", message.type, message.id, message.valueHB, message.valueLB);
                 OPENTHERM::send(master_out_pin_, message); // send message back to thermostat
-                ESP_LOGD(LOGTOPIC, "---> sent to thermostat");//: %1", message.id);
+                //ESP_LOGD(LOGTOPIC, "---> sent to thermostat");//: %1", message.id);
                 mode = MODE_LISTEN_MASTER;
             }
             else if (OPENTHERM::isError())
