7bd497cbdcbdf65beb4be8b87d7787ca36a43375
marek.fekete <marek.fekete@siemens.healthineers.com>
2023-10-02 15:51:38 +0200

    cpp

--- components/openthermgw/openthermgw.h
+++ components/openthermgw/openthermgw.h
@@ -32,72 +32,20 @@ class OpenthermGW: public PollingComponent
     sensor::Sensor *slave_in_pin_sensor = new sensor::Sensor();
     sensor::Sensor *slave_out_pin_sensor = new sensor::Sensor();
 
-    void set_master_in_pin(uint8_t pin) { master_in_pin_ = pin; }
-    void set_master_out_pin(uint8_t pin) { master_out_pin_ = pin; }
-    void set_slave_in_pin(uint8_t pin) { slave_in_pin_ = pin; }
-    void set_slave_out_pin(uint8_t pin) { slave_out_pin_ = pin; }
+    void set_master_in_pin(uint8_t pin);
+    void set_master_out_pin(uint8_t pin);
+    void set_slave_in_pin(uint8_t pin);
+    void set_slave_out_pin(uint8_t pin);
     
-    OpenthermGW(): PollingComponent(10000)
-    {
-    }
+    OpenthermGW();
 
-    static void IRAM_ATTR mHandleInterrupt()
-    {
-        mOT->handleInterrupt();
-    }
+    static void IRAM_ATTR mHandleInterrupt();
+    static void IRAM_ATTR sHandleInterrupt();
+    static void processRequest(unsigned long request, OpenThermResponseStatus status);
 
-    static void IRAM_ATTR sHandleInterrupt()
-    {
-        sOT->handleInterrupt();
-    }
-
-    static void processRequest(unsigned long request, OpenThermResponseStatus status)
-    {
-        ESP_LOGD(LOGTOPIC, "Opentherm request [request: %d]", request);
-        unsigned long response = mOT->sendRequest(request);
-        if (response)
-        {
-            Serial.println("B" + String(response, HEX)); // slave/boiler response
-            sOT->sendResponse(response);
-            ESP_LOGD(LOGTOPIC, "Opentherm response [response: %d, status %s", response, sOT->statusToString(status));
-        }
-    }
-
-    void setup() override
-    {
-        // This will be called once to set up the component
-        // think of it as the setup() call in Arduino
-        ESP_LOGD(LOGTOPIC, "Setup");
-
-        master_in_pin_sensor->publish_state(master_in_pin_);
-        master_out_pin_sensor->publish_state(master_out_pin_);
-        slave_in_pin_sensor->publish_state(slave_in_pin_);
-        slave_out_pin_sensor->publish_state(slave_out_pin_);
-
-        mOT = new OpenTherm(slave_in_pin_, slave_out_pin_); // master OT is paired with SLAVE pins (thermostat)
-        sOT = new OpenTherm(master_in_pin_, master_out_pin_, true);
-
-        mOT->begin(mHandleInterrupt);
-        sOT->begin(sHandleInterrupt, processRequest);
-/*        mOT->begin([this]()
-            {
-                mOT->handleInterrupt();
-            });
-        sOT->begin([this]()
-            {
-                sOT->handleInterrupt();
-            }, this->processRequest); */
-    }
-
-    void update() override
-    {
-        ESP_LOGD(LOGTOPIC, "update");
-    }
-
-    void loop() override
-    {
-        sOT->process();        
-    }
+    void setup() override;
+    void update() override;
+    void loop() override;
 };
 
 
