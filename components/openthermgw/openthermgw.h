1fd614176ab1e86b96fb1f141c35769c3cfe66f6
marek.fekete <marek.fekete@siemens.healthineers.com>
2023-10-02 14:55:47 +0200

    lambdas void

--- components/openthermgw/openthermgw.h
+++ components/openthermgw/openthermgw.h
@@ -73,11 +73,11 @@ class OpenthermGW: public PollingComponent
 
 //        mOT->begin(this->mHandleInterrupt);
 //        sOT->begin(this->sHandleInterrupt, this->processRequest);
-        mOT->begin([this](void)
+        mOT->begin([this](void*)()
             {
                 mOT->handleInterrupt();
             });
-        sOT->begin([this](void)
+        sOT->begin([this](void*)()
             {
                 sOT->handleInterrupt();
             }, this->processRequest);
