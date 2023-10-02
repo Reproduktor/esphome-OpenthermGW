d5d699277534ba04b735abc3b45a6cf622ef3896
marek.fekete <marek.fekete@siemens.healthineers.com>
2023-10-02 22:19:55 +0200

    sensro back

--- components/openthermgw/openthermgw.cpp
+++ components/openthermgw/openthermgw.cpp
@@ -58,7 +58,7 @@ namespace openthermgw {
     void OpenthermGW::update()
     {
         ESP_LOGD(LOGTOPIC, "update");
-        //this->sensor_temp_boiler->publish_state(28.3);
+        this->sensor_temp_boiler->publish_state(28.3);
     }
 
     void OpenthermGW::loop()
