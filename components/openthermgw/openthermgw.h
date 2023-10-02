e6d4767c63f04f86de78e345f464da54e39f2f8d
marek.fekete <marek.fekete@siemens.healthineers.com>
2023-10-02 23:51:30 +0200

    path?

--- components/openthermgw/openthermgw.h
+++ components/openthermgw/openthermgw.h
@@ -1,6 +1,6 @@
 #include "esphome.h"
 #include "esphome/components/sensor/sensor.h"
-#include "components/binary_sensor/binary_sensor.h"
+#include "binary_sensor/binary_sensor.h"
 
 #include <OpenTherm.h>
 
