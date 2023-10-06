18debd6514495d653e1146735cc421b367367822
marek.fekete <marek.fekete@siemens.healthineers.com>
2023-10-06 23:16:10 +0200

    async await

--- components/local_switch/__init__.py
+++ components/local_switch/__init__.py
@@ -7,13 +7,13 @@ local_switch_ns = cg.esphome_ns.namespace('local_switch')
 LocalSwitch = local_switch_ns.class_('LocalSwitch', switch.Switch, cg.Component)
 
 LOCAL_SWITCH_SCHEMA = switch.SWITCH_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(LocalSwitch)
+    cv.GenerateID(): cv.declare_id(LocalSwitch),
 }).extend(cv.COMPONENT_SCHEMA)
 
-def to_code(config):
+async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
-    yield switch.register_switch(var, config)
+    await cg.register_component(var, config)
+    await switch.register_switch(var, config)
 
 def local_switch_schema():
     schema = {
