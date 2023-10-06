4d4dc073a73e1d923773b61fd564ddc5000cd590
marek.fekete <marek.fekete@siemens.healthineers.com>
2023-10-06 23:00:59 +0200

    import

--- components/openthermgw/__init__.py
+++ components/openthermgw/__init__.py
@@ -1,6 +1,6 @@
 CODEOWNERS = ["@reproduktor/esphome-openthermgw"]
 
-#from esphome.local_switch import *
+from local_switch import *
 #from components.local_switch import LOCAL_SWITCH_SCHEMA
 import esphome.codegen as cg
 import esphome.config_validation as cv
@@ -94,7 +94,7 @@ CONFIG_SCHEMA = cv.Schema(
             device_class=DEVICE_CLASS_EMPTY,
             state_class=STATE_CLASS_MEASUREMENT).extend(),
         
-        cv.Optional(CONF_SWITCH_DHW_PUMP_OVERRIDE):  local_switch.LOCAL_SWITCH_SCHEMA(
+        cv.Optional(CONF_SWITCH_DHW_PUMP_OVERRIDE):  LOCAL_SWITCH_SCHEMA(
         ).extend(),
 
 #        cv.Optional(CONF_SWITCH_DHW_PUMP_OVERRIDE_MODE): switch.switch_schema(
