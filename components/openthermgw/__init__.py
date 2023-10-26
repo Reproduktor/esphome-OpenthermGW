c34e3c17fb31b4a44fccac78ae63191d8742de50
marek.fekete <marek.fekete@siemens.healthineers.com>
2023-10-26 22:55:59 +0200

    use the template number directly

--- components/openthermgw/__init__.py
+++ components/openthermgw/__init__.py
@@ -77,13 +77,10 @@ CONF_SCHEMA_SIMPLE_SWITCH = cv.maybe_simple_value(
         key=CONF_NAME,
     )
 
-CONF_SCHEMA_NUMERIC_INPUT = cv.maybe_simple_value(
-    number.CONFIG_SCHEMA(
-        SimpleNumber,
-       
-        ),
-        key=CONF_NAME,
-    )
+#CONF_SCHEMA_NUMERIC_INPUT = cv.maybe_simple_value(
+#    number.CONFIG_SCHEMA,
+#        key=CONF_NAME,
+#    )
 
 
 CONF_SENSOR_ACME_OT_OVERRIDE_BINARY_SWITCH_LIST = "acme_opentherm_override_binary_switches"
@@ -116,7 +113,7 @@ CONF_SCHEMA_ACME_OT_OVERRIDE_NUMERIC_SWITCH = cv.maybe_simple_value(
                 cv.Required(CONF_SENSOR_ACME_OT_MESSAGE_ID): cv.positive_int,
                 cv.Optional(CONF_SENSOR_ACME_OT_VALUE_ON_REQUEST, default='true'): cv.boolean,
                 cv.Optional(CONF_SENSOR_ACME_OT_VALUE_TYPE, default=0): cv.int_range(0, 7), # 0=u16, 1=s16, 2=f16, 3=u8LB, 4=u8HB, 5=s8LB, 6=s8HB, 7=REQUEST/RESPONSE
-                cv.Required(CONF_SENSOR_ACME_OT_OVERRIDE_NUMERIC_VALUE): CONF_SCHEMA_NUMERIC_INPUT,
+                cv.Required(CONF_SENSOR_ACME_OT_OVERRIDE_NUMERIC_VALUE): number.CONFIG_SCHEMA,
             }
         ),
         key=CONF_NAME,
